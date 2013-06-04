3c02cc1928fac00426ec20b4aaf488823974c7b0
Foo Bar <foo@bar.de>
2013-06-04 15:10:51 +0200

    CI by MK & KS Scorecard Command

---
+++ NerdGolfTracker/Operationen/ScorecardOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NerdGolfTracker.Operationen
+{
+    public class ScorecardOperation : Operation
+    {
+        private string c_Header = " Loch | Schläge" + Environment.NewLine;
+        public string FuehreAus(Scorecard scorecard)
+        {
+            String gesamtAnzeige = c_Header;
+            return gesamtAnzeige;
+        }
+
+    }
+}
