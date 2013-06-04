388ac0e5634cb95d387a1b6ea07f6690dd10d2d4
Foo Bar <foo@bar.de>
2013-06-04 13:46:08 +0200

    CI by KG & KS: add dummy result

--- NerdGolfTracker/EinfacheScorecard.cs
+++ NerdGolfTracker/EinfacheScorecard.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
+
 namespace NerdGolfTracker
 {
     public class EinfacheScorecard : Scorecard
     {
-
+        private Dictionary<int, int> ergebnis = new Dictionary<int, int>();
         public int AnzahlSchlaege { get; private set; }
         public int Lochnummer { get; private set; }
+        public Dictionary<int, int> ErgebnisZurueck
+        {
+            get { return ergebnis; }
+        }
 
         public EinfacheScorecard()
         {
+            ergebnis.Add(1,4);
+            ergebnis.Add(2, 5);
+            ergebnis.Add(3, 4);
             Lochnummer = 1;
         }
 
--- NerdGolfTracker/Scorecard.cs
+++ NerdGolfTracker/Scorecard.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
+
 namespace NerdGolfTracker
 {
     public interface Scorecard
     {
         int AnzahlSchlaege { get; }
         int Lochnummer { get; }
-
+        Dictionary<int, int> ErgebnisZurueck { get; }
         void ErhoeheAnzahlSchlaege();
         void SchliesseLochAb();
+
     }
 }
