1a7f044ff4c8cb6ef19bd7c4808d1e9bb95b6ef5
Foo Bar <foo@bar.de>
2013-06-05 09:59:35 +0200

    CI by KG & KS: scorecard output and test

--- NerdGolfTracker/Operationen/ScorecardOperation.cs
+++ NerdGolfTracker/Operationen/ScorecardOperation.cs
@@ -11,7 +11,18 @@ namespace NerdGolfTracker.Operationen
         public string FuehreAus(Scorecard scorecard)
         {
             String gesamtAnzeige = c_Header;
+            for (int i = 1; scorecard != null && i <= scorecard.ErgebnisZurueck.Count; i++)
+            {
+                gesamtAnzeige += GetFormattedStringForLine(i, scorecard.ErgebnisZurueck[i]);
+            }
+
             return gesamtAnzeige;
+        }
+
+        private string GetFormattedStringForLine(int lochNummer, int schlaege)
+        {
+            return lochNummer + " | " + schlaege + Environment.NewLine;
+
         }
 
     }
--- UnitTests/Operationen/ScorecardTest.cs
+++ UnitTests/Operationen/ScorecardTest.cs
@@ -35,12 +35,18 @@ namespace UnitTests.Operationen
         public void ScoreCard_ZeilenAusgabe()
         {
             _scorecard.ErhoeheAnzahlSchlaege();
+            _scorecard.SchliesseLochAb();
 
             string resultText = new ScorecardOperation().FuehreAus(_scorecard);
             var lines = resultText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-            var firstLine = lines.First();
-            Assert.That(firstLine, Contains.Substring("Loch"));
-            Assert.That(firstLine, Contains.Substring("Schläge"));
+            var secondLine = lines[1];
+            secondLine = secondLine.Replace(" ", string.Empty);
+
+            Assert.That(secondLine.StartsWith("1") && secondLine.EndsWith("1"), "Loch und Anzahl ist wrong");
+
+            var thirdLine = lines[2];
+            thirdLine = thirdLine.Replace(" ", string.Empty);
+            Assert.That(thirdLine.StartsWith("2") && thirdLine.EndsWith("0"), "Loch und Anzahl ist wrong");
         }
     }
 }
