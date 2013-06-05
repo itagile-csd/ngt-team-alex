287ba7a29a49ba8f85e64e5aed65c5ac6b40ed73
walter <walter@nix.com>
2013-06-05 14:58:59 +0200

    Merge

--- NerdGolfTracker/Befehle/HilfeBefehl.cs
+++ NerdGolfTracker/Befehle/HilfeBefehl.cs
@@ -4,9 +4,9 @@ namespace NerdGolfTracker.Befehle
 {
     public class HilfeBefehl : BefehlBasis
     {
-        public  HilfeBefehl():base(
-            
-        "Hilfe","h","zeigt Dir diese Erklaerung",new Hilfe())
+        public HilfeBefehl()
+            : base(
+                "Hilfe", "h", "zeigt Dir diese Erklaerung", new Hilfe(new AlleBefehle()))
         {
         }
     }
